e02b5960ccd6861e9e2f218b5997545ed17e5dcd
aatabong <64328055+aatabong@users.noreply.github.com>
2023-02-28 17:08:32 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+function openEmail() {
+  window.open("mailto:info@yourdomain.com")
+}
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAsp_ZsCR_fyhp5F9rblAxVKCLDZAlQuk8",
+//   authDomain: "iiuvigrader.firebaseapp.com",
+//   projectId: "iiuvigrader",
+//   storageBucket: "iiuvigrader.appspot.com",
+//   messagingSenderId: "414163516104",
+//   appId: "1:414163516104:web:2f7070a5b254e9763a83fd",
+//   measurementId: "G-E4JVE609F6"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+// Create code that when you click one of the buttons in the filter bar section it adds the image to the selected filters div. Only allow two from the filter-section-affinity div & filter-section-class div. Only allow one from the filter-section-tier div. Once the images are added, filter the cards in the card grid section by the data-affinity in the card class if it's a button from the filter-section-affinity class. Filter the cards in the card grid section by the data-class in the card class if it's a button from the filter-section-class class. Filter the cards in the card grid section by the data-tier in the card class if it's a button from the filter-section-tier class. Filter the cards in the card grid section by the data-illuvial when the user is inputting the illuvial name in the input from the  filter-section-search class.
+
---
+++ grading.js
@@ -0,0 +1,37 @@
+
+function addCardToDeckPreview() {
+  const selectedCardsDiv = document.querySelector('.selected-cards');
+  const card = event.target.closest('.card');
+  cardCount = selectedCardsDiv.childElementCount;
+
+    // check if there are already 30 cards selected
+    if (cardCount >= 30) {
+      alert('30 cards is the maximum amount per deck ');
+      return;
+    }
+
+  // Create a new card div to add to the selected cards div
+  const selectedCardDiv = document.createElement('div');
+  selectedCardDiv.classList.add('selected-card');
+  selectedCardDiv.dataset.src = card.dataset.src;
+  selectedCardDiv.dataset.illuvial = card.dataset.illuvial;
+  selectedCardDiv.dataset.affinity = card.dataset.affinity;
+  selectedCardDiv.dataset.tier = card.dataset.tier;
+  selectedCardDiv.dataset.class = card.dataset.class;
+
+  // Add the card image to the selected card div
+  const selectedCardImage = document.createElement('img');
+  selectedCardImage.src = card.dataset.src;
+  selectedCardDiv.appendChild(selectedCardImage);
+  
+  // Add the selected card div to the selected cards div
+  selectedCardsDiv.appendChild(selectedCardDiv);
+
+  // Remove the card from the selected cards div when clicked on
+  selectedCardDiv.addEventListener('click', () => {
+    selectedCardDiv.remove();
+  });
+}
+
+
+
