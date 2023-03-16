a965da75aef4c8a6a7810b7064d2ec0b92f18e65
aatabong <64328055+aatabong@users.noreply.github.com>
2023-03-16 15:02:45 -0500

    Update grading.js

--- grading.js
+++ grading.js
@@ -32,3 +32,56 @@ function addCardToDeckPreview() {
     selectedCardDiv.remove();
   });
 }
+
+// Select the filter-bar div and all the filter buttons
+const filterBar = document.querySelector('.filter-bar');
+const filterButtons = filterBar.querySelectorAll('.filter-button');
+
+// Select the filter-image div
+const filterImage = document.querySelector('.filter-image');
+
+// Set up variables to keep track of the number of images from each filter section
+let affinityCount = 0;
+let classCount = 0;
+let tierCount = 0;
+
+// Set up event listeners for each filter button
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Check the ID of the button to determine which filter section it belongs to
+    if (button.parentNode.classList.contains('filter-section-affinity')) {
+      // Check if the maximum number of images from the affinity filter section has been reached
+      if (affinityCount >= 2) {
+        return;
+      }
+      affinityCount++;
+      // Add a class to the new image element to indicate it came from the affinity filter section
+      const newImage = document.createElement('img');
+      newImage.src = button.querySelector('img').src;
+      newImage.classList.add('affinity');
+      filterImage.appendChild(newImage).setAttribute('id', button.id);
+    } else if (button.parentNode.classList.contains('filter-section-class')) {
+      // Check if the maximum number of images from the class filter section has been reached
+      if (classCount >= 2) {
+        return;
+      }
+      classCount++;
+      // Add a class to the new image element to indicate it came from the class filter section
+      const newImage = document.createElement('img');
+      newImage.src = button.querySelector('img').src;
+      newImage.classList.add('class');
+      filterImage.appendChild(newImage).setAttribute('id', button.id);
+    } else if (button.parentNode.classList.contains('filter-section-tier')) {
+      // Check if the maximum number of images from the tier filter section has been reached
+      if (tierCount >= 1) {
+        return;
+      }
+      tierCount++;
+      // Add a class to the new image element to indicate it came from the tier filter section
+      const newImage = document.createElement('img');
+      newImage.src = button.querySelector('img').src;
+      newImage.classList.add('tier');
+      filterImage.appendChild(newImage).setAttribute('id', button.id);
+    }
+  });
+});
